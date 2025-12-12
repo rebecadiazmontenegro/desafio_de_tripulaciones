1b180bbfc411b24543b154f543703517eaa41da6
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-12-12 21:50:54 +0100

    add/SignUp/Delete/getAll

---
+++ frontend/src/components/Main/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from './SignUpForm/SignUpForm'
+
+const SignUpAdmin = () => {
+  return  (
+    <section>
+      <SignUpForm />
+    </section>
+  );
+};
+
+export default SignUpAdmin;
---
+++ frontend/src/service/users.service.js
@@ -0,0 +1,21 @@
+const API_URL = import.meta.env.VITE_API_URL; 
+
+export const signUp = async (form, token) => {
+  try {
+    const response = await fetch(`${API_URL}/user/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, 
+      },
+      body: JSON.stringify(form),
+    });
+
+    const data = await response.json();
+
+    return { ok: response.ok, data };
+  } catch (error) {
+    console.error("Error en service:", error);
+    return { ok: false, data: { msg: "Error en el servidor" } };
+  }
+};
