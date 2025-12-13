2defe6ed7572826f29b6fb18153678502e5ca651
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-12-13 19:48:32 +0100

    intento

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import {Link, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
@@ -9,6 +9,11 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
+        <Link to="/signup">
+        <button className="showProfileButton">
+          SignUp
+        </button>
+      </Link>
         <Main />
       </BrowserRouter>
         <Footer />
