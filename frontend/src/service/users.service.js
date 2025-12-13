f70274e76c425bb5e0dd68f12918e52c9e814e1e
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-12-13 23:05:42 +0100

    intento

--- frontend/src/service/users.service.js
+++ frontend/src/service/users.service.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL; 
+const API_URL = import.meta.env.VITE_API_URL || "https://desafio-de-tripulaciones.onrender.com";
 
 console.log("❤️ API_URL en navegador:", import.meta.env.VITE_API_URL);
 
